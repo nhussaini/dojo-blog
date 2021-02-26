72bd7ead2eca78376b9b1cf5e7c07a880bfbff42
unknown <nhussaini@luc.edu>
2021-02-25 19:08:43 -0800

    added click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick=()=>{
+      console.log('Hello');
+    }
+
+    const handleClickAgain =(name)=>{
+      console.log('hello, '+name);
+    }
+
   return (
     <div className="Home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>click me!</button>
+      <button onClick={()=>handleClickAgain('nasr')}>click me!</button>
     </div>
     );
 }
